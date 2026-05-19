7544e036d1676db2aa18bff7632055978206be93
snowblind <shujaatnazir3139@gmail.com>
2026-05-19 12:39:03 +0530

    added send mail service class in the emailService branck , for now email is simple text , but later , i want to send a card via html/css , for now it is simple and it works

---
+++ src/main/java/org/shujaat/quoteEveryday/services/EmailService.java
@@ -0,0 +1,55 @@
+package org.shujaat.quoteEveryday.services;
+
+import org.shujaat.quoteEveryday.dto.QuoteDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Value("${mail.to}")
+    private String to;
+
+    @Value("${MAIL_FROM}")
+    private String from;
+
+    private final String subject = "Motivational quote of the Day!!";
+
+    private final GetQuoteService getQuoteService;
+
+    public EmailService(GetQuoteService getQuoteService) {
+        this.getQuoteService = getQuoteService;
+    }
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Scheduled(fixedDelay = 30000, initialDelay = 5000)
+    public void sendEmail() {
+        System.out.println("running send mail mehtod");
+
+        QuoteDto quote = getQuoteService.getQuote();
+
+        String body = quote.getQuote() + "\n" + quote.getAuthor();
+
+        try {
+
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setFrom(from);
+            mail.setSubject(subject);
+            mail.setText(body);
+
+            javaMailSender.send(mail);
+            System.out.println("Email Sent");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
--- src/main/java/org/shujaat/quoteEveryday/services/GetQuoteService.java
+++ src/main/java/org/shujaat/quoteEveryday/services/GetQuoteService.java
@@ -1,13 +1,13 @@
 package org.shujaat.quoteEveryday.services;
 
+import java.time.Duration;
+import java.util.List;
+
 import org.shujaat.quoteEveryday.dto.QuoteDto;
 import org.shujaat.quoteEveryday.entity.Quote;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @Service
 public class GetQuoteService {
@@ -18,19 +18,18 @@ public class GetQuoteService {
         this.webClient = webClient;
     }
 
-    public Mono<QuoteDto> getQuote() {
+    public QuoteDto getQuote() {
         return webClient
                 .get()
                 .uri("https://zenquotes.io/api/random")
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<List<Quote>>() {})
+                .bodyToMono(new ParameterizedTypeReference<List<Quote>>() {
+                })
+                .timeout(Duration.ofSeconds(5))
                 .map(q -> new QuoteDto(
                         q.get(0).getQuote(),
-                        q.get(0).getAuthor()
-                ));
+                        q.get(0).getAuthor()))
+                .block();
     }
-
-
-
 
 }
