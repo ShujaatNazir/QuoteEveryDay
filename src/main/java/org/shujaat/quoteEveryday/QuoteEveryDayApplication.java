71df44a78357ebe55e43f7bbe4e4c2bbd1d7f9aa
snowblind <shujaatnazir3139@gmail.com>
2026-05-19 10:51:45 +0530

    intial laryer of the project is done , added varibales to local file and simple layout is done

---
+++ src/main/java/org/shujaat/quoteEveryday/QuoteEveryDayApplication.java
@@ -0,0 +1,14 @@
+package org.shujaat.quoteEveryday;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class QuoteEveryDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteEveryDayApplication.class, args);
+    }
+}
---
+++ src/test/java/org/shujaat/quoteEveryday/QuoteEveryDayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shujaat.quoteEveryday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteEveryDayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
